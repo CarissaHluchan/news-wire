dad4918d0c69b1b391bde90fa7c83bc20e7c4c50
Carissa Hluchan <carissahluchan@gmail.com>
2024-09-27 15:01:58 -0600

    Add: routes to app.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,33 @@
+import { Route, Routes } from 'react-router-dom';
+
+// import Form from '../Form/Form';
+// import AllNewsView from '../AllNewsView/AllNewsView';
+// import DetailedView from '../DetailedView/DetailedView';
+// import ErrorPage from '../ErrorPage/ErrorPage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      News Wire
+      <header>
+        <h1>News Wire</h1>
+        {/* <Form /> */}
+      </header>
+      {/* <Routes>
+        <Route path='/' element={<AllNewsView />}/>
+        <Route path='article/:publishedAt'element={<DetailedView />}/>
+        <Route path='/error/:code' element={<ErrorPage />}></Route>
+        <Route path='*' element={<ErrorPage error={404} />}></Route>
+      </Routes> */}
+      <footer>
+        <div>Created By: Carissa Hluchan</div>
+        <a
+          href="https://github.com/CarissaHluchan"
+          target="_blank"
+          rel="noopener noreferrer"
+          className='url-link-github'
+        >See it on: GitHub</a>
+      </footer>
     </div>
   );
 }
